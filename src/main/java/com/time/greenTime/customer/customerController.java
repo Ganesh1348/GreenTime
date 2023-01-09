a2f50796ec23670cb61622a657299b2ef2930b29
GaneshHarikrishnan <ganesh.harikrishnan@bonbloc.com>
2023-01-09 21:02:54 +0530

    Added customer details

--- src/main/java/com/time/greenTime/customer/customerController.java
+++ src/main/java/com/time/greenTime/customer/customerController.java
@@ -1,6 +1,7 @@
 package com.time.greenTime.customer;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +13,10 @@ public class customerController
 	@GetMapping("/test")
 	public String getDetails() {
 		return "Hello...";
+	}
+	
+	@PostMapping("/hub")
+	public String getPost() {
+		return "Hello..";
 	}
 }
